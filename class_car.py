e41ec86ce67d4cbbb2308c48fb2fdd0a2717bcb9
Prasanna Gonapinuwala <prasannaanura@yahoo.com>
2025-04-30 23:51:33 -0600

    speling change

--- class_car.py
+++ class_car.py
@@ -2,7 +2,7 @@
 
 from car import Car
 car1 = Car("Toyota","Rav4",2024,"black",False)
-print("The Car Modrl is ",car1.make)
+print("The Car Model is ",car1.make)
 print("Model - ",car1.model)
 print("Year_",car1.year)
 print("sale:", car1.for_sale)
